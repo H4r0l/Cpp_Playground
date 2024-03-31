a21e9dd351225f08992ab8799c825f0f62dcdce0
Har0l <haroldurrea6@gmail.com>
2024-03-30 19:41:19 -0500

    Toll exercise and remainings

--- Homework_#10/Toll.cpp
+++ Homework_#10/Toll.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main() {
 
-    cout << "Calculadora de tarifas" << endl;
+    cout << "Calculadora de tarifas" << endl; // Enunciado del programa
     cout << "Autor: Harol Urrea | Codigo 1804606 |\nMecatronica C\n";
 
-    short int type = 0;
+    short int type = 0; // Declarar la variable para el tipo de vehiculo
 
-    cout << "Ingresa el tipo de vehiculo (1-7): "; 
+    cout << "Ingresa el tipo de vehiculo (1-7): ";  // MSJ Ingresar el tipo de vehiculo
     cout << "Camperos, automoviles y camionetas" << endl << "Buses y Microbuses" << endl << "Camiones" << endl << "Camiones de dos ejes" << endl << "Camiones de tres y cuatro ejes" << endl << "Camiones de cinco o mas ejes" << endl << "Ambulancia \n" << endl;
-    cin >> type;
+    // Enunciado opciones de vehiculos
+    cin >> type; // Ingresar el tipo de vehiculo
 
-    switch (type) {
+    switch (type) { // Switch para el tipo de vehiculo
         case 1:
             cout << "\tCategoria | Camperos, automoviles y camionetas: $7.700" << endl;
             break;
@@ -35,8 +36,8 @@ int main() {
         case 7:
             cout << "\tCategoria | Ambulancias: $15.000" << endl;
         default:
-            cout << "\tNo se reconoce el tipo de vehiculo" << endl;
+            cout << "\tNo se reconoce el tipo de vehiculo" << endl; // MSJ No se reconoce el tipo de vehiculo
     }
-    system("pause");
+    system("pause"); // Pausar el programa para que el usuario pueda ver el resultado
     return 0;
 }
