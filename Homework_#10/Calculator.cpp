a7fc43a8a01e025f3edfdea8c5b1b11a96116471
Har0l <haroldurrea6@gmail.com>
2024-03-30 19:35:17 -0500

    Calculator Exercise

--- Homework_#10/Calculator.cpp
+++ Homework_#10/Calculator.cpp
@@ -1,43 +1,44 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> // Libreria para las operaciones matematicas pow y sqrt
 
-using namespace std;
+using namespace std; 
 
 int main() {
 
-    cout << "Calculadora" << endl;
+    cout << "Calculadora" << endl; // Enunciado del programa
     cout << "Autor: Harol Urrea | Codigo 1804606 |\nMecatronica C\n";
 
-    short int num1, num2, option;
+    short int num1, num2, option; // Declarar variables para los 2 numeros de tipo entero corto 
 
-    cout << "Ingresa el primer numero: "; cin >> num1;
-    cout << "Ingresa el segundo numero: "; cin >> num2;
+    cout << "Ingresa el primer numero: "; cin >> num1; // Ingresa el primer numero
+    cout << "Ingresa el segundo numero: "; cin >> num2; // Ingresa el segundo numero
 
     cout << "Selecciona la operacion: " << endl << "1. Suma" << endl << "2. Resta" << endl << "3. Multiplicacion" << endl << "4. Division" << endl << "5. Potenciacion" << endl << "6. Radicacion" << endl << "Opcion: " << endl;
-    cin >> option;
+    // MSJ & Seleccionar la operacion a realizar
+    cin >> option; // Ingresa la operacion a realizar
 
-    switch(option) {
-        case 1:
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
+    switch(option) { // Switch para las 6 operaciones matematicas basicas
+        case 1: // Suma
+            cout << num1 << " + " << num2 << " = " << num1 + num2; // MSJ & Respuesta
+            break; 
+        case 2: // Resta
+            cout << num1 << " - " << num2 << " = " << num1 - num2; // MSJ & Respuesta
             break;
-        case 2:
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
+        case 3: // Multiplicacion
+            cout << num1 << " * " << num2 << " = " << num1 * num2; // MSJ & Respuesta
             break;
-        case 3:
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
-            break;
-        case 4:
-            if((num1 == 0) || (num2 == 0)) {
-                cout << "La division por cero siempre da cero";
-            } else {
-                cout << num1 << " / " << num2 << " = " << num1 / num2;
+        case 4: // Division
+            if((num1 == 0) || (num2 == 0)) { // Evaluar variable num2 para saber si es neutro
+                cout << "La division por cero siempre da cero"; // True
+            } else { // De lo contrario
+                cout << num1 << " / " << num2 << " = " << num1 / num2; // MSJ & Respuesta
             }
             break;
-        case 5:
-            cout << num1 << " ^ " << num2 << " = " << pow(num1, num2);
+        case 5: // Potenciacion
+            cout << num1 << " ^ " << num2 << " = " << pow(num1, num2); // MSJ & Respuesta
             break;
-        case 6:
-            cout << "La raiz cuadrada de " << num1 << " = " << sqrt(num1);
+        case 6: // Radicacion
+            cout << "La raiz cuadrada de " << num1 << " = " << sqrt(num1); // MSJ & Respuesta
             break;
         default:
             cout << "Opcion invalida";
