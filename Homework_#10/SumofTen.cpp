710e4dca02c1f4943df50d29cfbb9f1e3aab667a
Har0l <haroldurrea6@gmail.com>
2024-03-30 19:40:29 -0500

    Sum of ten numbers lmao

--- Homework_#10/SumofTen.cpp
+++ Homework_#10/SumofTen.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 int main() {
-    int num, positivos = 0, negativos = 0, ceros = 0, suma = 0;
+    int num, positivos = 0, negativos = 0, ceros = 0, suma = 0; // Declarar variables
 
-    for (int i = 0; i < 10; i++) {
-        cout << "Ingrese un numero: ";
-        cin >> num;
+    for (int i = 0; i < 10; i++) { // Iterar 10 veces para sumar 10 numeros 
+        cout << "Ingrese un numero: "; cin >> num; // MSJ & Asignar valor int por usuario
 
-        if (num > 0) {
-            positivos++;
-        } else if (num < 0) {
-            negativos++;
-        } else {
-            ceros++;
+        if (num > 0) { // Evaluar variable para identificar si es positivo 
+            positivos++; // True | MSJ & Sumar 1
+        } else if (num < 0) {  // Evaluar variable para identificar si es negativo
+            negativos++; // True | MSJ & Sumar 1
+        } else { // Evaluar variable para identificar si es cero
+            ceros++; // True
         }
 
-        suma += num;
+        suma += num; // Suma los 10 numeros
     }
     cout << "Hay " << positivos << " numeros positivos, " << negativos << " numeros negativos y " << ceros << " ceros en la suma. La suma total es " << suma << ".";
-
+    // Imprimir el resultado de la suma de los 10 numeros ingresados por el usuario y la suma total de todos los tipos de numeros
     return 0;
 }
