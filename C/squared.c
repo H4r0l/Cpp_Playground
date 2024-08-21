4df815bebe0d15c7c11ab30d4e0625376786a76d
Har0l <haroldurrea6@gmail.com>
2024-08-21 00:41:15 -0500

    Corrected

--- C/squared.c
+++ C/squared.c
@@ -2,20 +2,22 @@
 
 uint64_t square(uint8_t index)
 {
-    uint64_t res = 1;
-    uint64_t value = 0;
-    for(int i = 2; i < index; i++){
-        value = i*2;
-        res += value; 
+    if(index == 0){return 0;}
+    uint64_t value = 1;
+    // We multiply value by 2 (which is equivalent to doubling the grains) 
+    // index-1 times to calculate the grains on the given square.
+    for(int i = 1; i < index; i++){
+        value *= 2;
     }
-    if( index == 1){ return 1;}
-    return res;
+    return value;
 }
+
 uint64_t total(void)
 {
-    uint64_t res = 1;
-    for(int i = 1; i < 64; i++){
-        res += i*2;
+    uint64_t res = 0;
+    // We sum the grains on each square, from square 1 to square 64.
+    for(int i = 1; i <= 64; i++){
+        res += square(i);
     }
     return res;
 }
