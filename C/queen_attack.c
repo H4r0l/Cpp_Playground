350a4d157d305e08e87e6efa035ab999d76258be
Har0l <haroldurrea6@gmail.com>
2024-08-26 22:55:56 -0500

    Fix diagonal check

--- C/queen_attack.c
+++ C/queen_attack.c
@@ -4,12 +4,7 @@
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2)
 {
-    position_t diagonal1;
-    diagonal1.row = queen_1.row - queen_1.column; 
-    diagonal1.column = queen_1.column - queen_1.row; 
-    position_t diagonal2;
-    diagonal2.row = queen_2.row - queen_2.column; 
-    diagonal2.column = queen_2.column - queen_2.row; 
+
     if(queen_1.row > 8 || queen_2.row > 8 || queen_1.column > 8 || queen_2.column > 8 || queen_1 == queen_2){
         return (attack_status_t)INVALID_POSITION;
     }
