fd4294318edb06f5e58e6d0069d6830a2c982c5a
Har0l <haroldurrea6@gmail.com>
2024-08-26 23:19:13 -0500

    Corrected

--- C/queen_attack.c
+++ C/queen_attack.c
@@ -1,16 +1,18 @@
 #include "queen_attack.h"
-#include <stdbool.h>
-
+#include <stdlib.h>
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2)
 {
-
-    if(queen_1.row > 8 || queen_2.row > 8 || queen_1.column > 8 || queen_2.column > 8 || queen_1 == queen_2){
+    if (queen_1.row < 1 || queen_1.row > 8 || queen_1.column < 1 || queen_1.column > 8 ||
+    queen_2.row < 1 || queen_2.row > 8 || queen_2.column < 1 || queen_2.column > 8){
+    return (attack_status_t)INVALID_POSITION;
+    }    
+    else if(queen_1.row == queen_2.row && queen_1.column == queen_2.column){
         return (attack_status_t)INVALID_POSITION;
     }
     else if(queen_1.row == queen_2.row || queen_1.column == queen_2.column){
         return (attack_status_t)CAN_ATTACK; 
-    } else if(diagonal1.row == diagonal2.row && diagonal1.column == diagonal2.column){
+    } else if(abs(queen_1.row - queen_2.row) == abs(queen_1.column - queen_2.column)){
         return (attack_status_t)CAN_ATTACK;  
     }
     
