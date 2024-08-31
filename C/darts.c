3b3d2da31b34103bcd093c6f43ff8c796aaa79f1
Har0l <haroldurrea6@gmail.com>
2024-08-30 20:53:03 -0500

    Provisional Fix for an array

--- C/darts.c
+++ C/darts.c
@@ -2,22 +2,22 @@
 #include <stdint.h>
 #include <math.h>
 
-uint16_t find_distance(uint8_t x,uint8_t y)
+uint16_t find_distance(uint8_t coordinates_t[])
 {
-    uint16_t distance = sqrt(((x-0)^2)+((y-0)^2));
+    uint16_t distance = sqrt(((coordinates_t[0]-0)^2)+((coordinates_t[1]-0)^2));
     return distance;
 }
 
-uint16_t score(uint16_t x, uint16_t y)
+uint16_t score(uint16_t coordinates_t[0], uint16_t coordinates1_t])
 {
     
-    if(find_distance(x,y) > 10){
+    if(find_distance(coordinates_t[0],coordinates_t[1]) > 10){
         return 0;
-    } else if(find_distance(x,y) == 10){
+    } else if(find_distance(coordinates_t[0],coordinates_t[1]) == 10){
         return 1;
-    } else if(find_distance(x,y) >= 5){
+    } else if(find_distance(coordinates_t[0],coordinates_t[1]) >= 5){
         return 5;
-    } else if(find_distance(x,y) >=1){
+    } else if(find_distance(coordinates_t[0],coordinates_t[1]) >=1){
         return 10;
     }
     return 0;
