10118fe493c0fd3893420bc70204f123d8f2dd85
Har0l <haroldurrea6@gmail.com>
2024-09-03 23:48:16 -0500

    Headers update

--- C/darts.h
+++ C/darts.h
@@ -2,6 +2,7 @@
 #define DARTS_H
 #include <stdint.h>
 
-uint16_t score(uint16_t x, uint16_t y);
-uint16_t find_distance(uint8_t x,uint8_t y);
+uint16_t score(uint8_t coordinates[2]);
+uint16_t find_distance(uint8_t coordinates_t[2]);
+
 #endif
