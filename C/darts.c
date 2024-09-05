286df89c1946c401d9be43ae14808d640f0722ea
Har0l <haroldurrea6@gmail.com>
2024-09-04 21:46:56 -0500

    kinda working

--- C/darts.c
+++ C/darts.c
@@ -1,17 +1,16 @@
 #include "darts.h"
-#include <stdint.h>
 #include <math.h>
 
 // Function to calculate the distance from the origin to the given point
-uint16_t find_distance(uint8_t coordinates[2])
+uint16_t find_distance(coordinate_t coordinates)
 {
     // Calculate distance using Pythagorean theorem
-    uint16_t distance = (uint16_t)sqrt((coordinates[0] * coordinates[0]) + (coordinates[1] * coordinates[1]));
+    uint16_t distance = (uint16_t)sqrt((coordinates.x * coordinates.x) + (coordinates.y * coordinates.y));
     return distance;
 }
 
 // Function to calculate the score based on the coordinates
-uint16_t score(uint8_t coordinates[2])
+uint16_t score(coordinate_t coordinates)
 {
     uint16_t distance = find_distance(coordinates);  // Calculate the distance once
 
