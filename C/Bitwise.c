ec3f8f89657adad272031e309e2ed0e371755d5a
Har0l <haroldurrea6@gmail.com>
2025-01-14 22:04:13 -0500

    Bitwise completed

--- C/Bitwise.c
+++ C/Bitwise.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 //Complete the following function.
 
-
 void calculate_the_maximum(int n, int k) {
-  //Write your code here.
-  for(int i = 1; i < n; i++){
-    int max = 0;
-    
-    if((n & k) > max){max = n & k; printf("%d\n", max);}
-    else if((n | k) > max){max = n | k; printf("%d\n", max);}
-    else if((n ^ k) > max){max = n ^ k; printf("%d\n", max);}
-  }
+    int max_and = 0, max_or = 0, max_xor = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            int and_result = i & j;
+            int or_result = i | j;
+            int xor_result = i ^ j;
+
+            if (and_result < k && and_result > max_and) {
+                max_and = and_result;
+            }
+            if (or_result < k && or_result > max_or) {
+                max_or = or_result;
+            }
+            if (xor_result < k && xor_result > max_xor) {
+                max_xor = xor_result;
+            }
+        }
+    }
+
+    printf("%d\n%d\n%d\n", max_and, max_or, max_xor);
 }
 
 int main() {
