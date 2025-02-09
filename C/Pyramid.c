9ca1211ea1ed1b86e4cdeff92e1c0301264ae04c
Har0l <haroldurrea6@gmail.com>
2025-02-08 19:11:34 -0500

    Done

--- C/Pyramid.c
+++ C/Pyramid.c
@@ -5,14 +5,41 @@
 
 int main() 
 {
+
     int n;
     scanf("%d", &n);
-    for(int i = 0; i < n + (n - 1); i++){
-        for(int j = 0; j < n + (n - 1); j++){
-            printf("%d ", n );
+    
+        // Calculate matrix size
+    int size = 2 * n - 1;
+    
+    // Iterate through rows
+    for (int i = 0; i < size; i++) {
+        
+        // Iterate through columns
+        for (int j = 0; j < size; j++) {
             
+            // Calculate the minimum distance from any edge
+            int top = i;
+            int left = j;
+            int bottom = size - 1 - i;
+            int right = size - 1 - j;
+
+            // Ensure correct min_distance calculation
+            int min_distance = top;
+            if (left < min_distance) min_distance = left;
+            if (bottom < min_distance) min_distance = bottom;
+            if (right < min_distance) min_distance = right;
+
+            // Compute the number to print
+            int num = n - min_distance;
+
+            // Print the number with a space
+            printf("%d ", num);
         }
+        
+        // Move to the next line after finishing a row
         printf("\n");
     }
+    
     return 0;
 }
